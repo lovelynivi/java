730fdc08a4dbc71ef65fbf2c9443761ee3678fc1
admin <admin@admins-MacBook-Air.local>
2022-12-14 16:23:14 +0530

    com file to move SRC

--- SRC/com/learn/java/operators/LimitCheck1.java
+++ SRC/com/learn/java/operators/LimitCheck1.java
@@ -2,11 +2,17 @@ public class LimitCheck1{
  public static void main(String[] args){
  // assign a big value (exceeding the range) to the data type and note behaviour.
  // error occurs, note the error in program comments.
- int a = 2,147,483,6578;;
+ 
+ //error: <identifier> expected
+ int a = 2,147,483,6578;
+ //error: <identifier> expected.
  long b =  9,223,372,036,854,775,8079l; 
+ // error: floating-point number too large
  float c = 3.4e+0385f;
+ //floating-point number too large
  double d = 1.8e+3085;
  byte e = 129;
+ //error: <identifier> expected
  short f = 32,787;
  
  
